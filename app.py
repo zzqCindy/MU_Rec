79efa70edbe0c685d9b0733eca574f80c484f7ee
“zzqCindy” <zzcz4@mail.missouri.edu>
2019-03-31 01:23:43 -0500

    add local db

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request, jsonify, abort
-import json, warnings, os
+import json, warnings
+from bson import json_util
 warnings.filterwarnings('ignore')  # To ignore all warnings that arise here to enhance clarity
 from gensim.models.ldamodel import LdaModel
 from gensim.corpora.dictionary import Dictionary
 from nltk.tokenize import WordPunctTokenizer
+from database.abstract_topic import Abstract
 
 app = Flask(__name__)
 
-tasks = []
 stopwords = []
 
 with open("./model/ZZ/stopwords.txt", "r") as doc:
@@ -37,33 +38,21 @@ def index():
 def add_task():
     if not request.json or 'content' not in request.json:
         abort(400)
-    if len(tasks) > 0:
-        id = tasks[-1]['id']+1
-    else:
-        id = 1
     document = request.json['content']
     text = WordPunctTokenizer().tokenize(document)     # Extract tokens
     words = [dic.get(word, word) for word in text if (len(word) > 1 and word not in stopwords)]
     new_corpus = dictionary.doc2bow(words)
     vector = lda[new_corpus]
-    task = {
-        'id': id,
-        'content': document,
-        'value': str(vector)
-    }
-    tasks.append(task)
+    top_list = [s[1] for s in vector]
+    abstract = Abstract(document,top_list)
+    abstract.save()
     return jsonify({'result': 'success'})
 
 
 @app.route('/get_task/', methods=['GET'])
 def get_task():
-    if not request.args or 'id' not in request.args:
-        # 没有指定id则返回全部
-        return jsonify(tasks)
-    else:
-        task_id = request.args['id']
-        task = filter(lambda t: t['id'] == int(task_id), tasks)
-        return jsonify(task) if task else jsonify({'result': 'not found'})
+    tasks = Abstract.query_abstract()
+    return json.dumps(list(tasks), default=json_util.default)
 
 
 
---
+++ database/abstract_topic.py
@@ -0,0 +1,31 @@
+import pymongo
+
+# 返回一个 collection
+def get_coll():
+
+    client = pymongo.MongoClient('127.0.0.1', 27017)
+    db = client.test
+    abs_topic = db.abstract_topic
+
+    return abs_topic
+
+
+class Abstract(object):
+
+    def __init__(self, content, top_list):
+        self.content = content
+        self.top_list = top_list
+
+    def save(self):
+        abstract = {"content": self.content}
+        for i in range(0,len(self.top_list)):
+            abstract['topic%d'%i] = float(self.top_list[i])
+        print(abstract)
+        coll = get_coll()
+        id = coll.insert_one(abstract)
+        print(id)
+
+    @staticmethod
+    def query_abstract():
+        abstract = get_coll().find()
+        return abstract
